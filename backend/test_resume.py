f1b824dee03086a33bad2354abfdc619ee4798db
Vaishnavi10706 <vaishnavii10706@gmail.com>
2026-07-15 11:36:44 +0530

    Prepare project for deployment

--- backend/test_resume.py
+++
@@ -1,7 +0,0 @@
-from database import get_latest_resume
-resume = get_latest_resume()
-if resume:
-    print("Resume Found:\n")
-    print(resume[:500])   # print first 500 characters
-else:
-    print("No resume found.")
